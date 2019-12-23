d933a60143736d0d4da1eacc580f37df1e232caa
Ricardo <568082704@qq.com>
2019-12-23 23:25:19 +0800

    save

--- blog-1/src/router/blog.js
+++ blog-1/src/router/blog.js
@@ -19,14 +19,26 @@ const handleBlogRouter = (req,res)=>{
 
   //获取博客列表
   if (method==='GET'&&req.path==='/api/blog/list') {
-    const author = req.query.author || ''
+    let author = req.query.author || ''
     const keyword = req.query.keyword || ''
     // const listData = getList(author,keyword)
     // return new SuccessModel(listData)
+
+    if (req.query.isadmin) {
+      //管理员界面
+      const loginCheckResult = loginCheck(req)
+      if (loginCheckResult) {
+        //未登录
+        return loginCheckResult
+      }
+      //强制查询自己的博客
+      author = req.session.username
+    }
+    
     const result = getList(author,keyword) 
     return  result.then(listData=>{
-              return new SuccessModel(listData)
-            })
+      return new SuccessModel(listData)
+    })
   }
 
   //获取博客详情
@@ -46,7 +58,7 @@ const handleBlogRouter = (req,res)=>{
     const loginCheckResult = loginCheck(req)
     if (loginCheckResult) {
       //未登录
-      return loginCheck
+      return loginCheckResult
     }
 
     req.body.author = req.session.username
@@ -61,7 +73,7 @@ const handleBlogRouter = (req,res)=>{
     const loginCheckResult = loginCheck(req)
     if (loginCheckResult) {
       //未登录
-      return loginCheck
+      return loginCheckResult
     }
 
     const result = updateBlog(id,req.body)
@@ -79,7 +91,7 @@ const handleBlogRouter = (req,res)=>{
     const loginCheckResult = loginCheck(req)
     if (loginCheckResult) {
       //未登录
-      return loginCheck
+      return loginCheckResult
     } 
 
     const author = req.session.username
