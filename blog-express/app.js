6879c6cae823ecd44e1d599299dad2c99024e490
Ricardo <568082704@qq.com>
2019-12-27 23:59:28 +0800

    save

---
+++ blog-express/app.js
@@ -0,0 +1,42 @@
+var createError = require('http-errors')
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
+
+var indexRouter = require('./routes/index')
+var usersRouter = require('./routes/users')
+const blogRouter 
+
+var app = express()
+
+// view engine setup 和view相关的的暂时还用不到
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'))
+app.use(express.json()) //获取到post过来的数据，urlencoded也是
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, "public")))
+
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404))
+})
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'dev' ? err : {}
+
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error')
+})
+
+module.exports = app
